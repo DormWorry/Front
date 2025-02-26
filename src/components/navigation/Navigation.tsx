c2cf67afb6d076b2af5a0af3a2d5cfcee8b01a55
kwon <dohun_dev@naver.com>
2025-02-26 15:41:07 +0900

    feat: Navigation bar Component #Pretendard font 적용

---
+++ src/components/navigation/Navigation.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 15px 50px;
+  background-color: white;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+`
+
+const Logo = styled.img`
+  height: 30px;
+  align-self: center;
+`
+
+const NavLinks = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  list-style: none;
+  gap: 2rem;
+`
+
+const NavLink = styled.li`
+  font-size: 1rem;
+  font-family: 'Pretendard-bold';
+  cursor: pointer;
+  &:hover {
+    color: #00bfa5;
+  }
+`
+
+const Button = styled.button`
+  width: 150px;
+  height: 40px;
+  font-size: 15px;
+  font-family: 'Pretendard-bold';
+  background-color: #13cfb8;
+  color: white;
+  padding: 0.5rem 1rem;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  &:hover {
+    background-color: #00bfa5;
+  }
+`
+
+const Navigation: React.FC = () => {
+  return (
+    <Nav>
+      <Logo src="/logo.png" alt="Logo" />
+      <NavLinks>
+        <NavLink>홈</NavLink>
+        <NavLink>서비스 소개</NavLink>
+        <NavLink>주요 기능</NavLink>
+        <Button>회원가입 / 로그인</Button>
+      </NavLinks>
+    </Nav>
+  )
+}
+
+export default Navigation
--- src/pages/onboarding/index.tsx
+++ src/pages/onboarding/index.tsx
@@ -1,6 +1,9 @@
+import Navigation from '@/components/navigation/Navigation'
+
 export default function Onboarding() {
   return (
     <div>
+      <Navigation />
       <h1>Onboarding</h1>
     </div>
   )
