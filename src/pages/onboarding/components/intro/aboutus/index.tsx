83adb3e8cd2859d10032b1dfa7217321eec3f7de
kwon <dohun_dev@naver.com>
2025-02-27 17:47:17 +0900

    refactor: AboutUsComponent 간소화 및 분리된 컴포넌트 통합

--- src/pages/onboarding/components/intro/aboutus/index.tsx
+++ src/pages/onboarding/components/intro/aboutus/index.tsx
@@ -1,55 +1,41 @@
 import React from 'react'
 import * as S from './aboutus-styles'
-import BoxModel from '../../../../../components/box-model'
-import {
-  topRowServices,
-  bottomRowServices,
-} from '../../../../../constants/onboard-dummy'
+import IntroSection from './IntroSection'
+import ServicesGrid from './ServicesGrid'
+import { useAboutUsRefs } from './useRefs'
+import useScrollAnimation from '../../../../../hooks/onboarding/aboutus-hooks/useScrollAnimation'
 
 const AboutUsComponent: React.FC = () => {
+  const {
+    containerRef,
+    introRef,
+    descriptionRef,
+    topRowRef,
+    bottomRowRef,
+    bottomCardRefs
+  } = useAboutUsRefs()
+
+  useScrollAnimation({
+    containerRef,
+    introRef,
+    descriptionRef,
+    topRowRef,
+    bottomRowRef,
+    bottomRowCardsRefs: bottomCardRefs,
+  })
+
   return (
-    <S.Container>
-      <S.TopSection>
-        <S.IntroGroup>
-          <S.ThinkingIcon src="/svgs/thinkingface.svg" alt="Thinking Icon" />
-          <S.IntroTitle>
-            해피투게더, <br />
-            어떤 서비스인가요?
-          </S.IntroTitle>
-        </S.IntroGroup>
-        <S.IntroDescription
-          dangerouslySetInnerHTML={{
-            __html:
-              '해피투게더는 기숙사 생활을 더욱 편리하고 즐겁게 만들기 위한 다양한 서비스를 제공합니다.<br/> 룸메이트 매칭부터 공동 배달 주문까지, 여러분의 기숙사 라이프를 더욱 풍요롭게 만들어 드립니다.',
-          }}
-        ></S.IntroDescription>
-      </S.TopSection>
+    <S.Container ref={containerRef}>
+      <IntroSection 
+        introRef={introRef} 
+        descriptionRef={descriptionRef} 
+      />
       
-      <S.ServicesGrid>
-        {/* 역삼각형 배치: 상단에 하나 */}
-        <S.TopServiceRow>
-          {topRowServices.map((service) => (
-            <BoxModel
-              key={service.id}
-              imgSrc={service.imgSrc}
-              title={service.title}
-              description={service.description}
-            />
-          ))}
-        </S.TopServiceRow>
-        
-        {/* 역삼각형 배치: 하단에 두개 */}
-        <S.BottomServiceRow>
-          {bottomRowServices.map((service) => (
-            <BoxModel
-              key={service.id}
-              imgSrc={service.imgSrc}
-              title={service.title}
-              description={service.description}
-            />
-          ))}
-        </S.BottomServiceRow>
-      </S.ServicesGrid>
+      <ServicesGrid 
+        topRowRef={topRowRef}
+        bottomRowRef={bottomRowRef}
+        bottomCardRefs={bottomCardRefs}
+      />
     </S.Container>
   )
 }
