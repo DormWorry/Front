b2795fa3239860451e6430fc8f2c2235b9292ed4
kwon <dohun_dev@naver.com>
2025-02-27 17:46:58 +0900

    feat: IntroSection 컴포넌트 분리로 AboutUs 구조 개선

---
+++ src/pages/onboarding/components/intro/aboutus/IntroSection.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import * as S from './aboutus-styles'
+import { DivRef } from './useRefs'
+
+interface IntroSectionProps {
+  introRef: DivRef
+  descriptionRef: DivRef
+}
+
+const IntroSection: React.FC<IntroSectionProps> = ({ introRef, descriptionRef }) => {
+  return (
+    <S.TopSection>
+      <S.IntroGroup ref={introRef}>
+        <S.ThinkingIcon src="/svgs/thinkingface.svg" alt="Thinking Icon" />
+        <S.IntroTitle>
+          해피투게더, <br />
+          어떤 서비스인가요?
+        </S.IntroTitle>
+      </S.IntroGroup>
+      <S.IntroDescription
+        ref={descriptionRef}
+        dangerouslySetInnerHTML={{
+          __html:
+            '해피투게더는 기숙사 생활을 더욱 편리하고 즐겁게 만들기 위한 다양한 서비스를 제공합니다.<br/> 룸메이트 매칭부터 공동 배달 주문까지, 여러분의 기숙사 라이프를 더욱 풍요롭게 만들어 드립니다.',
+        }}
+      />
+    </S.TopSection>
+  )
+}
+
+export default IntroSection
