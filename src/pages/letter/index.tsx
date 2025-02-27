8bff9467280b572bdd7b6b0394f6106c95d215b1
kwon <dohun_dev@naver.com>
2025-02-28 01:20:56 +0900

    refactor: 편지 기능 메인 페이지 리팩토링 - 커스텀 훅으로 상태 관리 로직 분리 - 컴포넌트 간 결합도 낮추고 응집도 향상 - import 경로 수정 및 코드 정리

--- src/pages/letter/index.tsx
+++ src/pages/letter/index.tsx
@@ -1,7 +1,122 @@
+import React, { useState } from 'react'
+import { useRouter } from 'next/router'
+import LetterDetail from './LetterDetail'
+import * as S from './letter-styles'
+import { useLetterState } from '../../hooks/letter'
+import {
+  MailboxLayout,
+  ProfileSidebar,
+  MailboxTabs,
+  LetterList,
+  ComposeSection,
+} from './components'
+
+/**
+ * 편지 기능 메인 컴포넌트
+ */
 export default function LetterFeature() {
+  const router = useRouter()
+  const [imageError, setImageError] = useState(false)
+
+  // 사용자 상태 관리 훅 호출
+  const {
+    activeTab,
+    isComposing,
+    selectedLetter,
+    replyTo,
+    receivedLetters,
+    sentLetters,
+    needsReceivedScroll,
+    needsSentScroll,
+    isReceivedScrolledToBottom,
+    isSentScrolledToBottom,
+    receivedLetterListRef,
+    sentLetterListRef,
+    handleLetterClick,
+    handleComposeClick,
+    handleComposeSubmit,
+    handleReplyClick,
+    handleReturnToMailbox,
+    handleCloseDetail,
+    handleTabChange,
+    handleReceivedScroll,
+    handleSentScroll,
+  } = useLetterState()
+
+  const handleGoBack = () => {
+    router.push('/main')
+  }
+
+  const handleImageError = () => {
+    setImageError(true)
+  }
+
   return (
-    <div>
-      <h1>letter</h1>
-    </div>
+    <MailboxLayout onGoBack={handleGoBack}>
+      {/* 프로필 영역 - 왼쪽 사이드바 */}
+      <ProfileSidebar
+        roomNumber="1205호"
+        userName="권도훈"
+        avatarSrc="/logo.png"
+        onComposeClick={handleComposeClick}
+        onImageError={handleImageError}
+        imageError={imageError}
+      />
+
+      {/* 컨텐츠 영역 - 오른쪽 메인 */}
+      <S.ContentContainer>
+        <S.Header>
+          <h1>마음의 편지함</h1>
+        </S.Header>
+
+        {isComposing ? (
+          <ComposeSection
+            onReturn={handleReturnToMailbox}
+            onSubmit={handleComposeSubmit}
+            initialRecipient={replyTo}
+          />
+        ) : (
+          <S.TabContainer>
+            <MailboxTabs activeTab={activeTab} onTabChange={handleTabChange} />
+
+            {/* 받은 편지함 - 탭이 active일 때만 보여줌 */}
+            {activeTab === 'received' && (
+              <LetterList
+                letters={receivedLetters}
+                onLetterClick={handleLetterClick}
+                needsScroll={needsReceivedScroll}
+                onScroll={handleReceivedScroll}
+                isScrolledToBottom={isReceivedScrolledToBottom}
+                isEmptyMessage="아직 받은 편지가 없습니다."
+                ref={receivedLetterListRef}
+              />
+            )}
+
+            {/* 보낸 편지함 - 탭이 active일 때만 보여줌 */}
+            {activeTab === 'sent' && (
+              <LetterList
+                letters={sentLetters}
+                onLetterClick={handleLetterClick}
+                needsScroll={needsSentScroll}
+                onScroll={handleSentScroll}
+                isScrolledToBottom={isSentScrolledToBottom}
+                isEmptyMessage="아직 보낸 편지가 없습니다."
+                ref={sentLetterListRef}
+              />
+            )}
+          </S.TabContainer>
+        )}
+      </S.ContentContainer>
+
+      {/* 편지 상세 모달 */}
+      {selectedLetter && (
+        <LetterDetail
+          letter={selectedLetter}
+          onClose={handleCloseDetail}
+          onReply={handleReplyClick}
+          isReceived={activeTab === 'received'}
+        />
+      )}
+    </MailboxLayout>
   )
 }
