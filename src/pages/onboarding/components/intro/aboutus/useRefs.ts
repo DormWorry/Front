1af67e019aac410dcf63eff56c7e59a34ad52891
kwon <dohun_dev@naver.com>
2025-02-27 17:46:50 +0900

    feat: AboutUs 컴포넌트용 ref 관리를 위한 커스텀 훅 생성

---
+++ src/pages/onboarding/components/intro/aboutus/useRefs.ts
@@ -0,0 +1,25 @@
+import { useRef } from 'react'
+
+export type DivRef = React.RefObject<HTMLDivElement | null>
+
+export const useAboutUsRefs = () => {
+  const containerRef = useRef<HTMLDivElement | null>(null)
+  const introRef = useRef<HTMLDivElement | null>(null)
+  const descriptionRef = useRef<HTMLDivElement | null>(null)
+  const topRowRef = useRef<HTMLDivElement | null>(null)
+  const bottomRowRef = useRef<HTMLDivElement | null>(null)
+  
+  const bottomCardRef1 = useRef<HTMLDivElement | null>(null)
+  const bottomCardRef2 = useRef<HTMLDivElement | null>(null)
+  
+  const bottomCardRefs: DivRef[] = [bottomCardRef1, bottomCardRef2]
+
+  return {
+    containerRef,
+    introRef,
+    descriptionRef,
+    topRowRef,
+    bottomRowRef,
+    bottomCardRefs,
+  }
+}
