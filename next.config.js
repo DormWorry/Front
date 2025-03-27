cbfc91577f89cc6a5457e54d68402f5b7ce95a0f
kwon <dohun_dev@naver.com>
2025-03-27 13:23:48 +0900

    auth

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://www.dormworry.p-e.kr/api/:path*',
+      },
+      {
+        source: '/auth/:path*',
+        destination: 'https://www.dormworry.p-e.kr/auth/:path*',
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
